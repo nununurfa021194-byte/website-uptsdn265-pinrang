dc9e8f8d33a26e9f39c82c77c1ba8a91f6518c12
Nurul Fadhillah Iftitah Tahir <nununurfa021194@gmail.com>
2026-07-05 02:23:38 +0800

    Website resmi UPT SDN 265 Pinrang versi 1.0

---
+++ src/data/galeri.js
@@ -0,0 +1,115 @@
+export const galeri = [
+  {
+    judul: "Literasi Al-Qur'an",
+    folder: "literasi-alquran",
+    cover: "/images/galeri/literasi-alquran/001-yasinan.jpeg",
+    jumlah: 4,
+    icon: "fa-book-quran",
+    foto: [
+      "001-yasinan.jpeg",
+      "002-yasinan.jpeg",
+      "003-yasinan.jpeg",
+      "004-yasinan.jpeg"
+    ]
+  },
+
+  {
+    judul: "Pembelajaran",
+    folder: "pembelajaran",
+    cover: "/images/galeri/pembelajaran/001-belajar.jpeg",
+    jumlah: 2,
+    icon: "fa-graduation-cap",
+    foto: [
+      "001-belajar.jpeg",
+      "002-belajar.jpeg"
+    ]
+  },
+
+  {
+    judul: "Asesmen",
+    folder: "asesmen",
+    cover: "/images/galeri/asesmen/001-anbk.jpg",
+    jumlah: 7,
+    icon: "fa-clipboard-check",
+    foto: [
+  "001-anbk.jpg",
+  "002-anbk.jpg",
+  "003-anbk.jpg",
+  "004-anbk.jpg",
+  "005-anbk.jpg",
+  "006-anbk.jpg",
+  "007-anbk.jpg"
+]
+  },
+
+  {
+    judul: "Kerja Bakti",
+    folder: "kerja-bakti",
+    cover: "/images/galeri/kerja-bakti/001-kerja-bakti.jpg",
+    jumlah: 1,
+    icon: "fa-seedling",
+    foto: [
+  "001-kerja-bakti.jpg"
+]
+  },
+
+  {
+    judul: "Olahraga",
+    folder: "olahraga",
+    cover: "/images/galeri/olahraga/001-olahraga.jpg",
+    jumlah: 2,
+    icon: "fa-futbol",
+    foto: [
+  "001-olahraga.jpg",
+  "002-olahraga.jpg"
+]
+  },
+
+  {
+    judul: "Lomba",
+    folder: "lomba",
+    cover: "/images/galeri/lomba/001-lomba.jpg",
+    jumlah: 5,
+    icon: "fa-trophy",
+    foto: [
+  "001-lomba.jpg",
+  "002-lomba.jpg",
+  "003-lomba.jpg",
+  "004-lomba.jpg",
+  "005-lomba.jpg"
+]
+  },
+
+  {
+    judul: "Perayaan",
+    folder: "perayaan",
+    cover: "/images/galeri/perayaan/001-perayaan.jpg",
+    jumlah: 4,
+    icon: "fa-calendar-days",
+    foto: [
+  "001-perayaan.jpg",
+  "002-perayaan.jpg",
+  "003-perayaan.jpg",
+  "004-perayaan.jpg"
+]
+  },
+
+  {
+    judul: "Dokumentasi Lainnya",
+    folder: "dokumentasi-lainnya",
+    cover: "/images/galeri/dokumentasi-lainnya/001-terimaraport.jpg",
+    jumlah: 9,
+    icon: "fa-images",
+    foto: [
+  "001-terimaraport.jpg",
+  "002-terimaraport.jpg",
+  "003-terimaraport.jpg",
+  "004-terimaraport.jpg",
+  "005-terimaraport.jpg",
+  "006-terimaraport.jpg",
+  "007-terimaraport.jpg",
+  "008-terimaraport.jpg",
+  "009-terimaraport.jpg"
+]
+  }
+];
